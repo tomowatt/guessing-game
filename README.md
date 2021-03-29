7f4c8a9e5b4902017b7acfb28e510e46ad0f34f4
tom <tomowatt@me.com>
2021-03-29 19:55:46 +0100

    Updated with following Command line apps in Rust tutorial
    * Allows game to have variety of difficulties 
    * Updated Formatting
    * Corrected winning the game to exit rather than 'break' resulting in losing statement
    * Made the print statements more game styled

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## About
 
-Following the [The Rust Programming Language](https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html) to get a deeper understanding and practising writing in Rust.
+Following the [The Rust Programming Language](https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html) and [Command line apps in Rust](https://rust-cli.github.io/book/index.html) to get a deeper understanding and practising writing in Rust.
 
 ## Learnings
 
@@ -11,3 +11,4 @@ Following the [The Rust Programming Language](https://doc.rust-lang.org/book/ch0
 * Using external Crates
 * Using Cargo to setup packages
 * Using the `std` libraries to get Input
+* Creating an CLI to interact more with the game
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,87 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use structopt::StructOpt;
+
+/// Set Difficulty via CLI
+#[derive(StructOpt)]
+struct Cli {
+    /// Difficulty of the Game: "easy", "medium", "hard"
+    #[structopt(default_value = "easy", long)]
+    difficulty: String,
+}
+
+fn print_difficulty(difficulty: &str) {
+    println!("Playing on: {}", difficulty);
+}
 
 fn main() {
+    println!("Welcome to the Guessing Game");
+
+    let args = Cli::from_args();
+
+    let difficulty = match &args.difficulty as &str {
+        "easy" => {
+            print_difficulty(&args.difficulty);
+            (10, 101)
+        }
+        "medium" => {
+            print_difficulty(&args.difficulty);
+            (5, 201)
+        }
+        "hard" => {
+            print_difficulty(&args.difficulty);
+            (3, 301)
+        }
+        _ => {
+            print_difficulty("easy");
+            (10, 101)
+        }
+    };
 
     let min = 0;
-    let max = 101;
-    let mut tries = 5;
+    let (mut tries, max) = difficulty;
+
     let secret_number = rand::thread_rng().gen_range(min..max);
-    
-    println!("Welcome to the Guessing Game");
-   
+
     while tries != 0 {
         let mut guess = String::new();
         println!("Please select a Number between {} and {}", min, (max - 1));
         println!("You have {} tries left", tries);
-        io::stdin().read_line(&mut guess).expect("error reading input");
-        
-        let guess : u32 = match guess.trim().parse() {
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("error reading input");
+
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
         match guess.cmp(&max) {
             Ordering::Less => match guess.cmp(&secret_number) {
-                Ordering::Less => {println!("Less"); tries = tries - 1;},
-                Ordering::Greater => {println!("Greater"); tries = tries - 1;},
+                Ordering::Less => {
+                    println!("Need to Guess Higher!");
+                    tries = tries - 1;
+                }
+                Ordering::Greater => {
+                    println!("Guess Lower!");
+                    tries = tries - 1;
+                }
                 Ordering::Equal => {
-                    println!("Equal");
-                    break;
+                    println!("You Win!");
+                    std::process::exit(0);
                 }
             },
             Ordering::Greater => {
-                println!("{} is over {}, you'll never win", guess, (max -1));
+                println!("{} is over {}, you'll never win", guess, (max - 1));
                 tries = tries - 1;
-                },
+            }
             Ordering::Equal => {
-                println!("{} is over {}, you'll never win", guess, (max -1)); tries = tries - 1;},
+                println!("{} is over {}, you'll never win", guess, (max - 1));
+                tries = tries - 1;
+            }
         };
-    };
+    }
     println!("You Lose!");
+    println!("The Correct Answer: {}", secret_number);
 }
